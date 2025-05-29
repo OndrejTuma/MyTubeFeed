5ba1b2812854518709a9eef4f1ff19dcb9d1ffd2
Ondřej Tůma <ondrej.tuma@gmail.com>
2025-05-29 18:24:43 +0200

    chore: update compose file and nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: {
-    turbo: true
-  },
   images: {
     domains: ['yt3.ggpht.com', 'i.ytimg.com'],
   },
